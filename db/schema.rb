eae5f6c1475669720a544546e4f2323a47056ade
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-02-29 18:51:51 +0900

    owner_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_171556) do
+ActiveRecord::Schema.define(version: 2020_02_29_094055) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_171556) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "owner_id", null: false
   end
 
   create_table "taggings", force: :cascade do |t|
