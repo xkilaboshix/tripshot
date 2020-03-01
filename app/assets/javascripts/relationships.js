10c5adba4d3a30a3a095c2c2b6232bc7be54c8a4
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-01 18:45:34 +0900

    tab fix

--- app/assets/javascripts/relationships.js
+++ app/assets/javascripts/relationships.js
@@ -2,16 +2,34 @@
 // All this logic will automatically be available in application.js.
 
 $(function() {
-	//クリックしたときにfunctionが発火
-	$('ul.tab_area li').click(function() {
-		//indexでクリックされたタブが何番目かを調べる
-		var index = $('ul.tab_area li').index(this);
-		//非表示
-		$('.content_area').css('display','none');
-		//クリックされたタブの内容表示
-		$('.content_area').eq(index).fadeIn();
-		$('.tab_area li').removeClass('select');
-		$(this).addClass('select')
-	});
+  // 別ページから特定のタブにダイレクトリンク
+  var hash = location.hash;
+  //hashで取得した文字列から#より後を取得
+  if (hash) {
+    var tabname = hash.slice(1);
+  } else {
+    var tabname = "tab1";
+  }
+  //コンテンツを一度すべて非表示
+  $(".content_area").hide();
+  //一度タブについているクラスselectを消す
+  $(".tab_area li").removeClass("select");
+  $("#" + tabname).addClass("select");
+  var area_num = tabname.slice(3);
+  var area_name = "area" + area_num;
+  $("#" + area_name).show();
+
+
+	// タブをクリックしたときに発火
+  $("ul.tab_area li").click(function() {
+    var tab_id = $(this).attr("id"); //tab1
+    var area_id = "area" + tab_id.slice(3);
+    //非表示
+    $(".content_area").hide();
+    //クリックされたタブの内容表示
+    $("#" + area_id).show();
+    $(".tab_area li").removeClass("select");
+    $(this).addClass("select");
+  });
 });
 
