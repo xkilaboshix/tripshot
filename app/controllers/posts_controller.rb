50efbfcf03c4cdb6be9ec00ff337351e53be6c83
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-02 06:10:13 +0900

    週間お気に入りランキング実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,21 +3,25 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    @favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+    @favorite_ranks = week_post_calculate[0..2]
   end
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
     redirect_to list_path(@post.list_id)
   end
+
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
   end
+
   def edit
     @post = Post.find(params[:id])
   end
+
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
@@ -26,13 +30,16 @@ class PostsController < ApplicationController
       render 'edit'      
     end
   end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to list_path(@post.list_id)
   end
 
+
   private
+
   def post_params
     params.require(:post).permit(:list_id, :image, :body)
   end
@@ -42,5 +49,21 @@ class PostsController < ApplicationController
     if current_user.id != post.user_id
       redirect_to user_path(current_user)
     end
+  end
+  
+    # 一週間のお気に入りランキングを計算
+  def week_post_calculate
+    today = Date.today
+    past_date = today - 6
+    
+    # 全てのお気に入りランキングをとってくる
+    favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    week_rank = []
+    favorite_ranks.each do |post|
+      if post.created_at > past_date
+        week_rank += Post.where(created_at: post.created_at)
+      end
+    end  
+    return  week_rank
   end
 end
