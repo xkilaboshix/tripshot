1db04a2978907467381d72ebe3a2f99c99d1eb0e
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-03 20:53:34 +0900

    検索機能実装

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,28 @@
+class SearchsController < ApplicationController
+  
+  def search
+    if params[:search] == ""
+      redirect_back(fallback_location: posts_path)
+    elsif params[:select] == "ユーザー"
+      @users = User.where(['name LIKE ?', "%#{params[:search]}%"])
+    elsif params[:select] == "リスト名"
+      @lists = List.where(['title LIKE ?', "%#{params[:search]}%"]).pluck(:id)
+      session[:list_id] = @lists
+      redirect_to search_list_path
+    elsif params[:select] == "リストのタグ"
+      @lists = List.tagged_with([params[:search]], :any => true).pluck(:id)
+      session[:list_id] = @lists
+      redirect_to search_tag_path
+    end
+  end
+
+  def search_list
+    @lists = List.where(id: session[:list_id])
+    session[:list_id].clear
+  end
+
+  def search_tag 
+    @lists = List.where(id: session[:list_id])
+    session[:list_id].clear
+  end
+end
