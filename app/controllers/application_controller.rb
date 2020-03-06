78c540928e4a07dbcf3bc3b37cdcb60039aaee03
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-07 00:09:39 +0900

    レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:is_enabled, :name, :profile_image_id, :background_image_id, :introduction])
   end
   def after_sign_in_path_for(resource)
-    if current_user
       flash[:notice] = "ログインに成功しました" 
-      posts_path  # 指定したいパスに変更
-    else
-      flash[:notice] = "新規登録完了しました。プロフィールを編集してください" 
-      edit_user_path(current_user)  # 指定したいパスに変更
-    end
+      user_path(current_user)  # 指定したいパスに変更
   end
 end
