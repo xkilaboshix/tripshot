2ccea806cc9993830b6126042522e5784a3d3f52
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-08 02:19:58 +0900

    name バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   enum achievement: [:旅見習い, :旅好き, :旅マスター, :果てなき冒険者]
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 1..8 }
   
   # emailを保存する前に小文字に
   before_save { self.email = email.downcase }
