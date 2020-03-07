388b17f98ce8e36674264b29f80e37497c11a2de
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-07 14:30:32 +0900

    fix

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function(){
+  $($('#file')).on('change', $('#file'), function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+ 
+    reader.onload = function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,          
+          width: "100%"
+        }));
+      };
+    }(file);
+    reader.readAsDataURL(file);   
+  });
+});
