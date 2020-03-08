29656354cc759bfc5deac324b80181414eeb8016
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-08 17:09:07 +0900

    rspec user_model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 # 保存確認
 RSpec.describe User, type: :model do
+  # テスト用写真用意
   let(:image_path) { File.join(Rails.root, 'spec/fixtures/image.jpg') }
-  context "when data is saved correctly" do
+  context "data is valid" do
 
     before do
       @user = User.new
@@ -16,13 +17,13 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = 111111
     end
 
-    it "is valid with is_enabled, name, profile_image_id, background_image_id, achievement, email, password, password_confirmation" do
+    it "with is_enabled, name, profile_image_id, background_image_id, achievement, email, password, password_confirmation" do
       expect(@user).to be_valid
     end
 
   end
   # 名前存在確認
-  context "when data is not saved correctly" do
+  context "data is invalid" do
     before do
       @user = User.new
       @user.is_enabled = true
@@ -35,13 +36,13 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = 111111
     end
 
-    it "is not saved because no name has been entered" do
+    it "because no name has been entered" do
       expect(@user).to be_invalid
       expect(@user.errors[:name]).to include("は1文字以上で入力してください")
     end
   end
   # 名前8文字以内テスト
-  context "when data is not saved correctly" do
+  context "data is invalid" do
     before do
       @user = User.new
       @user.is_enabled = true
@@ -53,10 +54,30 @@ RSpec.describe User, type: :model do
       @user.password = 111111
       @user.password_confirmation = 111111
     end
-    it "is not saved because the name has not been entered within 8 characters" do
+    it "because name has not been entered within 8 characters" do
       expect(@user).to be_invalid
       expect(@user.errors[:name]).to include("は8文字以内で入力してください")
     end
+  end
+  context "data is invalid" do
+
+    before do
+      @user = User.new
+      @user.is_enabled = true
+      @user.name = "テスト"
+      @user.profile_image_id = Rack::Test::UploadedFile.new(image_path)
+      @user.background_image_id = Rack::Test::UploadedFile.new(image_path)
+      @user.achievement = "旅見習い"
+      @user.email = "testtesttest"
+      @user.password = 111111
+      @user.password_confirmation = 111111
+    end
+
+    it "because email has not been entered correctly" do
+      expect(@user).to be_invalid
+      expect(@user.errors[:email]).to include("は不正な値です")
+    end
+
   end
 end
 
