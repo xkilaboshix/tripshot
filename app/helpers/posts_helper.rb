65c4a3073a6de8822b355de1c04e68212d62e449
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-12 20:07:58 +0900

    fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,12 @@
 module PostsHelper
-  # def rank_image_url(post_id)
-  #   case post_id
-  #   when @first_rank
-  #       'first_place.png'
-  #   when @second_rank
-  #       'second_place.png'
-  #   when @third_rank
-  #       'third_place.png'
-  #     end
-  # end
+  def rank(post,first_rank,second_rank,third_rank)
+    file_name = 'no-image.png'
+    if post.id == first_rank.id
+      file_name = 'first_place.png'
+    elsif post.id == second_rank.id
+      file_name = 'second_place.png'
+    elsif post.id == third_rank.id
+      file_name = 'third_place.png'
+    end
+  end
 end
