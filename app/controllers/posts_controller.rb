6a2675ab6665e5f74ebf8914507896fd5f98605e
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-12 11:48:04 +0900

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,11 +75,10 @@ class PostsController < ApplicationController
     # 全てのお気に入りランキングをとってくる
     favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
     week_rank = []
-    rank_index = 1
+
     favorite_ranks.each do |post|
       if post.created_at > past_date
         week_rank += Post.where(created_at: post.created_at)
-        rank_index += 1
       end
     end  
     return  week_rank
