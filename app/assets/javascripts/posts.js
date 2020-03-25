95237806424d9d5eeef8db8e798b3512e7f95572
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-26 04:48:50 +0900

    remove jscroll

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -20,16 +20,3 @@ $(function(){
     reader.readAsDataURL(file);   
   });
 });
-
-// 無限スクロール
-$(window).on("scroll", function() {
-  scrollHeight = $(document).height();
-  scrollPosition = $(window).height() + $(window).scrollTop();
-  if ((scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
-    //スクロールの位置が下部5%の範囲に来た場合
-    $(".jscroll").jscroll({
-      contentSelector: ".jscroll",
-      nextSelector: "a.page-link:last"
-    });
-  }
-});
