61a1d6bf3fb857a0bbb8c1bc748e81b3e568f611
佐藤　魁登 <kaitosato@satouisaotounoMacBook-Pro.local>
2020-03-27 17:56:57 +0900

    リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,15 @@
 class UsersController < ApplicationController
 before_action :correct_user, except: [:show]
+before_action :set_user
 
   def show
-    @user = User.find(params[:id])
     @lists = List.where(user_id: @user.id)
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(current_user)
     else
@@ -20,11 +18,9 @@ before_action :correct_user, except: [:show]
   end
 
   def withdraw
-    @user = User.find(params[:id])
   end
 
   def switch
-    @user = User.find(params[:id])
     if @user.update(is_enabled: false)
       sign_out current_user 
     end
@@ -34,6 +30,10 @@ before_action :correct_user, except: [:show]
   private
   def user_params
     params.require(:user).permit(:is_enabled, :name, :profile_image, :background_image, :introduction, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def correct_user
